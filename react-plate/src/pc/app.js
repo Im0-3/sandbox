7ee1126bf63490dabac0df6c2c2269d0efafe274
omi_yusuke <omi_yusuke@liginc.co.jp>
2016-05-16 18:30:35 +0900

    不要なファイルの削除

--- react-plate/src/pc/app.js
+++ react-plate/src/pc/app.js
@@ -1,5 +1,36 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './components/App';
+//import App from './components/App';
+
+
+class Form extends React.Component {
+    constructor(){
+        this.state = {
+            name: 'LIG'
+        };
+    }
+    render(){
+        return (
+            <div>
+                Hello { this.state.name }!
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    constructor(){
+        this.state = {
+            name: 'LIG'
+        };
+    }
+    render(){
+        return (
+            <div>
+                Hello { this.state.name }!
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
